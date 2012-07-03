e625c7b51c052cf72c64881f1ae3dfd9e2e6dbad
Dag Liodden <dag@tapad.com>
2012-07-03 17:14:54 -0400

    Codecs are stateful. Add logback logging

--- project/Build.scala
+++ project/Build.scala
@@ -17,9 +17,10 @@ object Scitrusleaf extends Build {
 
   val dependencies = Seq(
     "ch.qos.logback" % "logback-classic" % "0.9.24" % "runtime",
-    "com.twitter" %% "finagle-core" % "3.0.0",
-    "com.twitter" %% "finagle-stream" % "3.0.0",
-    "io.netty" % "netty" % "3.4.0.Alpha1",
+    "org.slf4j" % "slf4j-api" % "1.6.1",
+    "com.twitter" % "finagle-core" % "5.1.0",
+    "com.twitter" % "finagle-stream" % "5.1.0",
+    "io.netty" % "netty" % "3.5.0.Final",
     "org.specs2" %% "specs2" % "1.5" % "test"
   )
 
---
+++ src/main/scala/com/tapad/scitrusleaf/Logger.scala
@@ -0,0 +1,7 @@
+package com.tapad.scitrusleaf
+
+import org.slf4j.LoggerFactory
+
+package object protocol {
+  val Logger = LoggerFactory.getLogger("scitrusleaf")
+}
